e9679fbf1d0eec9f1ef2242e93496906a0f3ab44
eladtzemach <eladtzemach@gmail.com>
2018-07-22 20:25:54 -0400

    task2 - get() and exists() functions done

--- lww.py
+++ lww.py
@@ -14,7 +14,7 @@ class LWW:
     def add_to_set(lww_set, element, timestamp):
 
         """
-        If the element already exists in the lww_set, check its current timestamp.
+        If the element already exists in lww_set, check its current timestamp.
         If it is smaller than the one provided -> replace it.
         If it is bigger -> leave it.
         """
@@ -23,7 +23,7 @@ class LWW:
             if element_current_timestamp < timestamp:
                 lww_set[element] = timestamp
 
-        # If the element does not exist in the add_set, add it.
+        # if the element does not exist in lww_add, add it
         else:
             lww_set[element] = timestamp
 
@@ -40,3 +40,32 @@ class LWW:
             self.add_to_set(self.lww_remove, element, timestamp)
 
         return True
+
+    def exists(self, element):
+
+        # if element is not in lww_add
+        if element not in self.lww_add:
+            return False
+
+        # elif the element has a larger timestamp in lww_add
+        elif element in self.lww_add and element in self.lww_remove \
+                and self.lww_add[element] >= self.lww_remove[element]:
+            return True
+
+        # elif element is not in lww_remove (but it is in lww_add)
+        elif element not in self.lww_remove:
+            return True
+
+        else:
+            return False
+
+    def get(self):
+
+        lww_result = []
+
+        # for each element in lww_add, make sure it exists
+        for element in self.lww_add:
+            if self.exists(element):
+                lww_result.append(element)
+
+        return lww_result
